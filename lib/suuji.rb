113c89401a78f48674e6508890f34dadc44b0355
Jo Apichai <jo@oozou.com>
2015-09-17 11:15:51 +0700

    Add to_arabic conversion

--- lib/suuji.rb
+++ lib/suuji.rb
@@ -1,25 +1,13 @@
 require "suuji/version"
 
 module Suuji
-  DIGITS = {
-    '1' => '一',
-    '2' => '二',
-    '3' => '三',
-    '4' => '四',
-    '5' => '五',
-    '6' => '六',
-    '7' => '七',
-    '8' => '八',
-    '9' => '九'
-  }.freeze
-
   SUFFIXS = ['', '十', '百', '千'].freeze
   MYRIADS = ['', '万', '億', '兆', '京'].freeze
 
   def self.to_kanji(value)
     parts = value.to_s.reverse.scan(/.{1,4}/)
-    result = ''
 
+    result = ''
     parts.each_with_index do |part, index|
       result = "#{to_kanji_partial(part)}#{MYRIADS[index]}#{result}"
     end
@@ -27,23 +15,64 @@ module Suuji
     result
   end
 
-  # def self.to_decimal(value)
-  #   digits = DIGITS.invert
-  #   digits[value].to_i
-  # end
+  def self.to_arabic(value)
+    regex = ''
+    MYRIADS.each { |myriad| regex = "(?:(.*)#{myriad})?#{regex}" }
+
+    parts = Regexp.new(regex).match(value)
+
+    result = ''
+    (1..MYRIADS.length).each do |i|
+      result += to_arabic_partial(parts[i]) unless parts[i].nil?
+    end
+
+    result.to_i
+  end
 
   private
 
-  def self.to_kanji_partial(value)
-    result = ''
+    def self.to_kanji_partial(value)
+      digit_hash = Hash[arabic_numbers.zip(kanji_numbers)]
 
-    0.upto(value.length - 1).each do |i|
-      next unless value[i].to_i > 0
+      result = ''
+      0.upto(value.length - 1).each do |i|
+        next unless value[i].to_i > 0
 
-      num = (i > 0 && value[i] == '1') ? '' : DIGITS[value[i]]
-      result = "#{num}#{SUFFIXS[i]}#{result}"
+        number = (i > 0 && value[i] == '1') ? '' : digit_hash[value[i]]
+        result = "#{number}#{SUFFIXS[i]}#{result}"
+      end
+
+      result
     end
 
-    result
-  end
+    def self.to_arabic_partial(value)
+      digit_hash = Hash[kanji_numbers.zip(arabic_numbers)]
+
+      regex = ''
+      SUFFIXS.each { |suffix| regex = "(?:(.?)#{suffix})?#{regex}" }
+      digits = Regexp.new(regex).match(value)
+
+      result = ''
+      length = SUFFIXS.length
+      (1..length).each do |i|
+        case digits[i]
+        when nil
+          result += '0'
+        when ''
+          result += (i == length) ? '0' : '1'
+        else
+          result += digit_hash[digits[i]]
+        end
+      end
+
+      result
+    end
+
+    def self.arabic_numbers
+      %w(1 2 3 4 5 6 7 8 9)
+    end
+
+    def self.kanji_numbers
+      %w(一 二 三 四 五 六 七 八 九)
+    end
 end
--- spec/suuji_spec.rb
+++ spec/suuji_spec.rb
@@ -28,9 +28,26 @@ describe Suuji do
     end
   end
 
-  # describe '#to_decimal' do
-  #   it 'converts Japanese numerals to decimal' do
-  #     expect(Suuji.to_decimal('一')).to eq 1
-  #   end
-  # end
+  describe '#to_arabic' do
+    it 'converts Japanese numerals to decimal' do
+      expect(Suuji.to_arabic('一')).to eq 1
+      expect(Suuji.to_arabic('十')).to eq 10
+      expect(Suuji.to_arabic('二十')).to eq 20
+      expect(Suuji.to_arabic('百')).to eq 100
+      expect(Suuji.to_arabic('四百')).to eq 400
+      expect(Suuji.to_arabic('四百四')).to eq 404
+      expect(Suuji.to_arabic('千百十一')).to eq 1111
+      expect(Suuji.to_arabic('七千七百七十七')).to eq 7777
+      expect(Suuji.to_arabic('八千八十')).to eq 8080
+
+      expect(Suuji.to_arabic('一万')).to eq 1_0000
+      expect(Suuji.to_arabic('百万')).to eq 100_0000
+      expect(Suuji.to_arabic('千万')).to eq 1000_0000
+      expect(Suuji.to_arabic('十二万三千四百五十六')).to eq 12_3456
+      expect(Suuji.to_arabic('千二百三十四万五千六百七十八')).to eq 1234_5678
+
+      expect(Suuji.to_arabic('一京一兆一億一万')).to eq 1_0001_0001_0001_0000
+      expect(Suuji.to_arabic('二千三百四十五京二千三百四十五兆二千三百四十五億二千三百四十五万二千三百四十五')).to eq 2345_2345_2345_2345_2345
+    end
+  end
 end
